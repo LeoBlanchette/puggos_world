0df45e1de8c299f50ac73c6f73c7bec4dbdfa648
LB3D <85522485+LeoBlanchette@users.noreply.github.com>
2024-06-22 19:22:24 -0500

    Bug Fix

--- ui/building_interface_elements/scripts/button_mod_object.gd
+++ ui/building_interface_elements/scripts/button_mod_object.gd
@@ -6,6 +6,6 @@ var item_id: int = 0
 var scene_path: String = ""
 
 func _on_pressed() -> void:
-	UIBuildingInterface.set_active(false)
+	UIBuildingInterface.toggle_building_interface()
 	var mh = AssetLoader.asset_loader.get_mod_hierarchy(scene_path)
 	Cmd.cmd("/placement %s %s"%[mh["mod_type"], item_id])
