6a119a5e7af372425f9e119057fbe6492c8e0e0c
LB3D <85522485+LeoBlanchette@users.noreply.github.com>
2024-09-12 17:26:17 -0500

    Update player_count.gd

--- nodes/cmd/scripts/commands/player_count.gd
+++ nodes/cmd/scripts/commands/player_count.gd
@@ -6,3 +6,4 @@ func player_count(_command:ArgParser):
 	var player_count_label:String = Cmd.color_line("Player count", Color.GREEN)
 	var player_count_number:String = Cmd.color_line(str(Players.players.size()), Color.YELLOW)
 	Cmd.print_to_console("%s: %s"%[player_count_label, player_count_number])
+	
